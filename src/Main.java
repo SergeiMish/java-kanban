591118139fa0a568b6561fa171073ad897704827
SergeiMish <mishalevskii@yandex.ru>
2024-08-19 20:28:45 +0500

    Split 5 FT

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
 import manager.InMemoryTaskManager;
 import manager.TaskManager;
+import tasks.Epic;
+import tasks.Status;
+import tasks.SubTask;
+import tasks.Task;
 
 public class Main {
     public static void main(String[] args) {
         TaskManager taskManager = new InMemoryTaskManager();
-//        Task newTa = new Task("Имя", "Детали", Status.NEW);
-//        Task newTa2 = new Task("Имя", "Детали", Status.DONE);
-//        Epic epic1 = new Epic("Задача", "Детали задачи", Status.NEW);
-//        SubTask subTask = new SubTask("Подзадача", "Детали подзадачи", Status.NEW);
-//        SubTask subTask1 = new SubTask("Подзадача2", "Детали подзадачи2", Status.DONE);
-//        Epic epic2 = new Epic("Задача2", "Детали задачи2", Status.NEW);
-//        SubTask subTask2 = new SubTask("Подзача задачи2", "Детали подзадачи2", Status.NEW);
-//
+       Task task = new Task("Поход в магазин", "Покупка молока", Status.NEW);
+       Task task1 = new Task("Уборка", "Только кухня", Status.IN_PROGRESS);
+       Epic epic = new Epic("Поход в автосалон", "Тест драйв 3-х китайце", Status.NEW);
+       SubTask subTask1 = new SubTask("Haval", "в 10.00", Status.NEW, epic.getId());
+       SubTask subTask2 = new SubTask("Changan", "14.00", Status.NEW, epic.getId());
+       SubTask subTask3 = new SubTask("Cherry", "Лучше совсем не ходить", Status.NEW, epic.getId());
+
+       taskManager.createTask(task);
+       taskManager.createTask(task1);
+       taskManager.createEpic(epic);
+       taskManager.createSubTask(subTask1, epic);
+        taskManager.createSubTask(subTask2, epic);
+        taskManager.createSubTask(subTask3, epic);
+       taskManager.getListTasks();
+       taskManager.getSubTasks();
 
     }
 }
