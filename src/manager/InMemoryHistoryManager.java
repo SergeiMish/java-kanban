b220feff89cfbb19769b762367e5f3bcacc187f3
SergeiMish <mishalevskii@yandex.ru>
2024-09-15 16:02:42 +0500

    Split 6 FT

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -8,18 +8,18 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final int MAX_SIZE_MEMORY = 10;
+    private final int maxSizeInMemory = 10;
     private final LinkedList<Task> history = new LinkedList<>();
 
     @Override
     public void add(Task task) {
-        if (history.size() == MAX_SIZE_MEMORY) {
+        if (history.size() == maxSizeInMemory) {
             history.removeFirst();
         }
         history.add(task);
     }
-    void remove(int id){
 
+    void remove(int id){
     }
 
     @Override
