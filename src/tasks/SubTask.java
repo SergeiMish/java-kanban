9e5be768a7bc9ef78bc3a3f661c27818ca1c3546
SergeiMish <mishalevskii@yandex.ru>
2024-09-15 15:41:10 +0500

    Split 6 FT

--- src/tasks/SubTask.java
+++ src/tasks/SubTask.java
@@ -1,10 +1,13 @@
 package tasks;
 
 public class SubTask extends Task {
+
     private final Integer epicId;
+
     public int getEpicId() {
         return epicId;
     }
+
     public SubTask(String name, String detail, Status status, Integer epicId) {
         super(name, detail, status);
         this.epicId = epicId;
